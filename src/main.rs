4cea70a0ad295e0eed385bebb4c4cac3055df284
Yerin <yerin@arum3d.com>
2023-11-07 12:43:19 +0000

    feat: create a simple web server with actix_web

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,35 @@
-fn main() {
-    println!("Hello, world!");
+use actix_web::{get, middleware, web, App, HttpServer, HttpResponse, HttpRequest};
+
+#[get("/")]
+async fn index(req: HttpRequest) -> HttpResponse {
+    let query_string = req.query_string();
+
+    let query_params: Vec<(&str, &str)> = query_string
+        .split('&')
+        .map(|param| {
+            let parts: Vec<&str> = param.splitn(2, '=').collect();
+            (parts[0], parts.get(1).copied().unwrap_or(""))
+        })
+        .collect();
+
+    HttpResponse::Ok().body(format!("{:?}", query_params))
+}
+
+async fn not_found() -> HttpResponse {
+    HttpResponse::NotFound().body("404")
+}
+
+#[actix_web::main]
+async fn main() -> std::io::Result<()> {
+    env_logger::init_from_env(env_logger::Env::new().default_filter_or("debug"));
+
+    HttpServer::new(|| {
+        App::new()
+            .wrap(middleware::Logger::default())
+            .service(index)
+            .default_service(web::route().to(not_found))
+    })
+    .bind(("127.0.0.1", 8080))?
+    .run()
+    .await
 }
